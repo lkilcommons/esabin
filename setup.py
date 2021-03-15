423411338a35f1882a0b6e7c6d838b48079f3e14
Liam Kilcommons <liam.kilcommons27@gmail.com>
2021-03-15 13:06:00 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, Extension
 from setuptools.command import install as _install
 
 setup(name='esabin',
-      version = "0.2.0",
+      version = "0.2.1",
       description = "Bin geophysical data in equal solid angle bins",
       author = "Liam Kilcommons",
       author_email = 'liam.kilcommons@colorado.edu',
